1bffba9151eec1cfee84f3b5fad84d98ea6c4f2d
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-02 08:08:12 -0500

    created Node and LinkedList class

---
+++ src/LinkedList.java
@@ -0,0 +1,10 @@
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+    }
+
+}
