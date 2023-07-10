b3269f021d59bfc32c29f083da67e12d5226b055
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-10 13:46:41 -0500

    Added a problem

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -3,8 +3,28 @@ public class LinkedList {
     private Node tail;
     private int length;
 
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
     public LinkedList(int value) {
         Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       LinkedList myLinkedList = new LinkedList(4);
+
+//       Problem:
+//Given a linked list, write a function to reverse the nodes of the list in-place.
+//Example:
+//Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+//Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+//Note:
+//A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+//Return a deep copy of the list.
     }
 }
